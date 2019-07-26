6fc5553ce1753e8a70e438b928535ab002b99fed
LGerwin <50540902+LGerwin@users.noreply.github.com>
2019-07-26 18:31:17 +0800

    Main.py

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ from flask_mail import Mail, Message
 """Global Variables"""
 non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
 final_output = []
-final_header = ['Serial No',"Search Query","URL Link","Title of Article","Text Summary"]
+final_header = ['Serial No',"Search Query","URL Link","Title of Article","Text Summary","Abstract"]
+abstract_Txt = ""
 
 """Set up Selenium driver"""
 chrome_options = Options()
@@ -23,7 +24,8 @@ chrome_options.add_argument("-incognito")
 chrome_options.add_argument("--disable-popup-blocking")
 sel_driver = webdriver.Chrome(executable_path='./static/ChromeDriverWin32/chromedriver.exe',chrome_options=chrome_options)
 
-def scrape(lst_query,filename):
+def scrape(lst_query,withAbstract,filename):
+
     for query in lst_query:
         """Scrape scheduled link from Selenium"""
         url = "https://www.google.com/search?q=" + query
@@ -61,16 +63,19 @@ def scrape(lst_query,filename):
                 sub_output.append(url)
             """attempts to get URL content"""
             try:
-                results = get_content(url) #gets content from URL
+                results = get_content(url,withAbstract,filename) #gets content from URL
                 if results[1] != "":
                     sub_output.append(results[0]) #Title
                     sub_output.append(results[1]) #Main content
+                    if(counter==final_counter):
+                        abstract_Txt = abstractSummarize('Process.txt')
+                    sub_output.append(abstract_Txt)
                     final_output.append(sub_output)
                     counter += 1
             except:
                 continue
 
-    #Output to Excel File
+    # Output to Excel File
     df_results = pd.DataFrame(final_output, columns=final_header)
     writer = pd.ExcelWriter('./static/user_pulls/Output_'+filename+'.xlsx', engine='xlsxwriter')
     df_results.to_excel(writer, sheet_name='Results', header=final_header, index=False)
@@ -88,14 +93,14 @@ def scrape(lst_query,filename):
     worksheet.set_column('C:C', 40, format)
     worksheet.set_column('D:D', 20, format)
     worksheet.set_column('E:E', 70, format)
+    worksheet.set_column('E:E', 70, format)
     # Clear df
     df_results = None
     writer.save()
     writer.close()
 
-
 #Main content Generator with BS4 and Selenium if BS4 fails to scrape
-def get_content(url):
+def get_content(url,withAbstract,filename):
     headers = requests.utils.default_headers()
     headers.update({
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
@@ -145,8 +150,15 @@ def get_content(url):
                     temp = link.translate(non_bmp_map)
                 except:
                     continue
-
             results = results + temp
+
+    if(withAbstract):
+        #Save a text file with original text for abstract summary
+        with codecs.open("Process.txt","a",encoding='utf8') as f:
+            try:
+                f.write(results)
+            except(UnicodeEncodeError):
+                f.write(results.translate(non_bmp_map))
 
     #otherwise output with BS4 and summariser
     results = summarize(results)
@@ -156,6 +168,54 @@ def get_content(url):
     return final_text_summary
 
 
+def abstractSummarize(filepath):
+    with open("args.pickle", "rb") as f:
+        args = pickle.load(f)
+
+    print("Loading dictionary...")
+    word_dict, reversed_dict, article_max_len, summary_max_len = build_dict("valid", args.toy)
+    print("Loading validation dataset...")
+    valid_x = build_dataset("valid", word_dict, article_max_len, summary_max_len, filepath, args.toy)
+    valid_x_len = [len([y for y in x if y != 0]) for x in valid_x]
+
+    with tf.Session() as sess:
+        print("Loading saved model...")
+        model = Model(reversed_dict, article_max_len, summary_max_len, args, forward_only=True)
+        saver = tf.train.Saver(tf.global_variables())
+        ckpt = tf.train.get_checkpoint_state("./saved_model/")
+        saver.restore(sess, ckpt.model_checkpoint_path)
+
+        batches = batch_iter(valid_x, [0] * len(valid_x), args.batch_size, 1)
+
+        print("Writing summaries")
+        for batch_x, _ in batches:
+            batch_x_len = [len([y for y in x if y != 0]) for x in batch_x]
+
+            valid_feed_dict = {
+                model.batch_size: len(batch_x),
+                model.X: batch_x,
+                model.X_len: batch_x_len,
+            }
+
+            prediction = sess.run(model.prediction, feed_dict=valid_feed_dict)
+            prediction_output = [[reversed_dict[y] for y in x] for x in prediction[:, 0, :]]
+            # return line
+            abstract = ""
+            # Writing to text file
+            #with open("result.txt", "a") as f:
+
+            for line in prediction_output:
+                summary = list()
+                for word in line:
+                    if word == "</s>":
+                        break
+                    if word == "black" or word == "bosch" or word == "<" or word == "unk" or word == ">":
+                        continue
+                    if word not in summary:
+                        summary.append(word)
+                        abstract = abstract + " " + word
+        os.remove('./static/abst_file/Process'+filename+'.txt')
+        return abstract
 
 """-------------------------------FLASK APPLICATION------------------------------------"""
 app = Flask(__name__)
@@ -195,15 +255,20 @@ def send_mail():
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 @app.route('/', methods=['POST'])
 def scrape_now():
     #OBtains data from html form and pass it through python to another html page
     queries = request.form['queries'] #receives from html form as String
+    withAbstract = request.form['inc_exc']
     lst_queries = queries.split(',') #split by ','
     current_timestamp = datetime.now().strftime('%m%d%Y%H%M%S')
-    scrape(lst_queries,current_timestamp)
+    if(withAbstract=='True'):
+        withAbstract = True
+    else:
+        withAbstract = False
+    scrape(lst_queries,withAbstract,current_timestamp)
     return render_template('downloads.html', filename=current_timestamp)
 
 @app.route('/return-file/<filename>')

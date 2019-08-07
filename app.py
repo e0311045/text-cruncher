9edf9ce746d025b7edc1309a929b2ae9376f0356
LGerwin <50540902+LGerwin@users.noreply.github.com>
2019-08-07 19:12:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -253,6 +253,6 @@ def about():
 
 #runs the application in debug mode
 if __name__ == "__main__":
-    # port = int(os.environ.get('PORT', 5000))
-    # app.run(host='0.0.0.0', port=port)
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    # app.run(debug=True)
